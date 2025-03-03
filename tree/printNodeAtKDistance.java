2249a9264e2db1a3b776004b1ddcc5a7618650f2
netra21mohekar <148302146+netra21mohekar@users.noreply.github.com>
2025-03-03 23:07:04 +0530

    Update printNodeAtKDistance.java

--- tree/printNodeAtKDistance.java
+++ tree/printNodeAtKDistance.java
@@ -1,2 +1,40 @@
 public class printNodeAtKDistance {
+    class Node{
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+     void print(Node root,int k){
+        if(root==null)return;
+        if(k==0){
+            System.out.print(root.data+" ");
+        }
+        else{
+            print(root.left,k-1);
+            print(root.right,k-1);
+        }
+     }
+
+    public static void main(String[] args) {
+        printNodeAtKDistance p = new printNodeAtKDistance();
+
+        Node root = p.new Node(1);
+        root.left = p.new Node(2);
+        root.right = p.new Node(3);
+        root.left.left = p.new Node(4);
+        root.left.right = p.new Node(5);
+        root.right.left = p.new Node(6);
+        root.right.right = p.new Node(7);
+
+        p.print(root,2);
+
+
+    }
+
 }
